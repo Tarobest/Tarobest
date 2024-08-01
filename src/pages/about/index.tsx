c9ed8c67bf95c284e9f272e478e3b6c04caf3b0d
virtuosohy <virtuosohy@outlook.com>
2024-08-01 19:48:34 +0800

    feat：第一次

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,37 +1,41 @@
-import { View } from "@tarojs/components";
-import Taro, { useLoad } from "@tarojs/taro";
-import { useTranslation } from "react-i18next";
-import CustomTabBar from "../../component/cutom_tab_bar/custom_tab_bar";
-import ReactLogo from "../../component/ReactLogo/ReactLogo";
-import "./index.css";
+import { useLoad } from "@tarojs/taro";
+
+import "./index.scss";
 
 export default function Index() {
-  const { t } = useTranslation();
   useLoad(() => {
     console.log("Page loaded.");
-    Taro.setNavigationBarTitle({
-      title: t("main:About"),
-    });
   });
 
-  const tabItems = [
-    {
-      icon: "../../asset/icon/homeTab.png",
-      selectIcon: "../../asset/icon/homeTabSelect.png",
-      text: t("main:home"),
-    },
-    {
-      icon: "../../asset/icon/about.png",
-      selectIcon: "../../asset/icon/aboutSelect.png",
-      text: t("main:About"),
-    },
-  ];
-
   return (
-    <View className='index'>
-      <View className='box'>Tarobest+React</View>
-      <ReactLogo></ReactLogo>
-      <CustomTabBar tabItems={tabItems}></CustomTabBar>
-    </View>
+    // <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
+    <body className="bg-gray-100">
+    <header className="p-8 bg-white shadow-lg">
+      <h1 className="text-3xl font-bold">我的网站</h1>
+    </header>
+
+    <main className="p-8">
+      <section className="max-w-md mx-auto">
+        <h2 className="text-2xl font-bold mb-4">欢迎</h2>
+        <p className="text-gray-800 text-lg">
+          这是使用 Tailwind CSS 创建的简单页面。Tailwind 是一种功能类优先的 CSS 框架，用于快速 UI 开发。
+        </p>
+      </section>
+
+      <section className="max-w-md mx-auto mt-8">
+        <h2 className="text-2xl font-bold mb-4">了解更多</h2>
+        <p className="text-gray-800 text-lg">
+          如果你对 Tailwind CSS 感兴趣，可以访问他们的<a href="https://tailwindcss.com"
+                                                        className="text-blue-500 hover:underline">官方网站</a>了解更多信息。
+        </p>
+      </section>
+    </main>
+
+    <footer className="p-8 bg-gray-100 text-center">
+      <p className="text-gray-600">ls © 2024</p>
+    </footer>
+    </body>
+    // </link>
   );
 }
