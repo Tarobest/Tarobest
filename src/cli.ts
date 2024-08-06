98f041439e61d36ba73fa31ae940f40a12b3fee9
Jia <2812975682@qq.com>
2024-08-06 23:06:55 +0800

    模板选项提供形式更改

--- src/cli.ts
+++ src/cli.ts
@@ -5,10 +5,7 @@ import os from 'os'
 import path from "path"
 import simpleGit from "simple-git"
 import fs from 'fs'
-
 import ora from "ora"
-import chalk from "chalk"
-import templates from "./tempateInfo"
 
 const TEMPLATE_SRC = 'git@github.com:jia8708/Tarobest.git'
 
@@ -23,43 +20,33 @@ program
 const prompts = [
     {
       type: 'list',
-      name: 'framework',
-      message: '选择什么框架?',
-      choices: ['React', 'Vue'],
+      name: 'module',
+      message: '请选择模板',
+      choices: ['react','react-i18n','react-unocss','react-i18n-unocss','vue','vue-i18n','vue-unocss','vue-i18n-unocss'],
     },
     {
       type: 'confirm',
-      name: 'i18n',
-      message: '是否使用i18n?',
+      name: 'renameFolder',
+      message: '是否需要为模板重命名？',
+      default: false
     },
     {
-      type: 'confirm',
-      name: 'unocss',
-      message: '是否使用unocss?',
-    },
+      type: 'input',
+      name: 'folderName',
+      message: '请输入文件夹名字：',
+      when: (answers:{[x:string]:any}) => answers.renameFolder // 仅当用户选择重命名时显示此问题
+    }
   ];
   inquirer.prompt(prompts  as any[]).then(async answers => {
   console.log('用户选择:', answers);
   const spinner = ora()
   spinner.start('正在匹配...');
 
-  const localPath = path.join(os.tmpdir(), 'taro-repo'); // 使用临时目录作为本地路径
+  const localPath = path.join(os.tmpdir(), 'tarobest-repo'); // 使用临时目录作为本地路径
 
    // 确保 localPath 目录存在  
    if (!fs.existsSync(localPath)) {  
     fs.mkdirSync(localPath, { recursive: true });  
-  }  
-
-  function findFirstMatchingTemplateKey(answer:{[x:string]:any}, templates:{[x:string]:any}) {  
-    // 使用 Object.keys 和 find 来找到第一个匹配的模板键  
-    const matchingKey = Object.keys(templates).find(key => {  
-      const template = templates[key];  
-      // 检查 answer 的每个属性是否都与 template 的对应属性相等  
-      return Object.keys(answer).every(prop => answer[prop] === template[prop]);  
-    });  
-    
-    // 直接返回找到的匹配模板键  
-    return matchingKey as string;  
   }  
     
     // 使用 simpleGit 克隆仓库
@@ -70,19 +57,20 @@ const prompts = [
       await git.clone(TEMPLATE_SRC, '.', ['--no-checkout'])
 
       // 调用函数并打印结果  
-      const branch = findFirstMatchingTemplateKey(answers, templates); 
+      const branch = answers['module']
       await git.checkout(branch);
-  
-      console.log(`找到匹配的分支: ${branch}`);
 
+      const targetFolderName = answers.renameFolder ? answers.folderName : branch;
+      const targetPath = path.join(process.cwd(), targetFolderName);
+  
       spinner.start('正在克隆...')
-      const targetPath = path.join(process.cwd(), `${branch}`); // 假设你想克隆到当前工作目录
+
       await git.clone(TEMPLATE_SRC, targetPath, ['--single-branch', '--branch', branch, '--no-tags']);
       console.log(`分支 ${branch} 已克隆到 ${targetPath}`);
       spinner.stop()
       
     } catch (error) {
-      console.error(chalk.red('发生错误:'), error);
+      console.error('发生错误:', error);
     } finally {
       spinner.stop()
       // 清理临时仓库目录
