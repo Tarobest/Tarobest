527fc6290a8f46e4432125b6210cb09565e4fedf
xiyang830 <3275519905@qq.com>
2024-08-08 17:21:05 +0800

    feat: 新增国际化演示

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,28 +1,35 @@
-
 import { useLoad } from "@tarojs/taro";
+import { useTranslation } from "react-i18next";
+import Button from "../../component/Button/Button";
+import i18n from "../../i18n";
 import "./index.scss";
 
 export default function Index() {
-
   useLoad(() => {
     console.log("Page loaded.");
   });
+  const { t } = useTranslation();
 
   return (
-
-    <div className="container">
+    <div className='container'>
       <header>
-        <h1>欢迎访问我们的网站</h1>
+        <h1>{t("main:Welcome")}</h1>
       </header>
 
       <section>
-        <h2>内容</h2>
-        <p>这里是服务内容的介绍。根据屏幕宽度不同，这段文字的布局会有所变化。</p>
+        <Button
+          hansleClick={() => {
+            i18n.changeLanguage(i18n.language === "en" ? "zh" : "en");
+          }}
+        >
+          {t("main:changeLanguage")}
+        </Button>
+        <h2>{t("main:contentTitle")}</h2>
+        <p>{t("main:content")}</p>
       </section>
       <footer>
         <p>&copy; ls</p>
       </footer>
     </div>
-
   );
 }
