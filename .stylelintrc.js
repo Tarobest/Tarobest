3c11ea29e26f982f25f2e69ef1b6670296228e83
Huayang Tang <tanghuayang@xiaohongshu.com>
2024-08-11 19:31:54 +0800

    feat: 添加vscode-stylelint配置文件

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,27 +1,26 @@
 module.exports = {
   extends: [
-    'stylelint-config-standard-scss',
-    'stylelint-config-recommended-scss',
-    'stylelint-config-idiomatic-order'
+    "stylelint-config-standard-scss",
+    "stylelint-config-recommended-scss",
+    "stylelint-config-idiomatic-order",
   ],
-  plugins: ['stylelint-order'],
+  plugins: ["stylelint-order"],
   ignoreFiles: [
-    '**/*.js',
-    '**/*.ts',
-    '**/*.jsx',
-    '**/*.tsx',
-    '**/*.json',
-    '**/*.md',
-    '**/*.yaml'
+    "**/*.js",
+    "**/*.ts",
+    "**/*.jsx",
+    "**/*.tsx",
+    "**/*.json",
+    "**/*.md",
+    "**/*.yaml",
   ],
   rules: {
     // 禁止在具有较高优先级的选择器后出现被其覆盖的较低优先级的选择器
-    'no-descending-specificity': null,
+    "no-descending-specificity": null,
     "selector-class-pattern": null, //类选择器命名规则
     "selector-id-pattern": null, //id选择器命名规则
     "selector-type-no-unknown": null, //未知的选择器
     "at-rule-no-unknown": null, //未知的规则
     "unit-no-unknown": null,
-    "lightness-notation": null,
-  }
-}
+  },
+};
