88151ab2ca3f8638d25a86ac9ddb062bf01d0a92
jia8708 <14483673+jia8708@user.noreply.gitee.com>
2024-08-21 11:29:19 +0800

    package.json改变时自动构建&prettier代码风格应用

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,16 +2,23 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    rules:{
-      "@typescript-eslint/no-explicit-any":"warn"
-    }
-  }
-  
+	{ files: ["**/*.{js,mjs,cjs,ts}"] },
+	{ languageOptions: { globals: globals.browser } },
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	{
+		rules: {
+			"@typescript-eslint/no-explicit-any": "warn",
+			"no-undef": "off",
+			"no-tabs": ["error", { allowIndentationTabs: true }],
+			indent: ["error", "tab", { ObjectExpression: "off" }],
+			"space-before-function-paren": ["error", "always"],
+			quotes: ["error", "double"],
+			curly: ["error", "multi-or-nest", "consistent"],
+			semi: ["error", "always"],
+			"no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
+			"@typescript-eslint/no-require-imports": "off"
+		}
+	}
 ];
