b9a4063ca49b947846f61f76dfbf9cf1468122fa
xiyueyezibile <2859439384@qq.com>
2024-08-27 17:18:26 +0800

    feat: update 权限

--- src/cli.ts
+++ src/cli.ts
@@ -4,98 +4,124 @@ import os from "os";
 import path from "path";
 import simpleGit from "simple-git";
 import fs from "fs";
+
 import ora from "ora";
+import chalk from "chalk";
 
-const TEMPLATE_SRC = "https://github.com/Tarobest/Tarobest.git";
+const TEMPLATE_SRC = "git@github.com:jia8708/Tarobest.git";
 
-program.name("tarobest");
+program.executableDir("../src/commands");
 
-program
-	.command("pull")
-	.description("拉取模板")
-	.action(async () => {
-		const prompts = [
-			{
-				type: "list",
-				name: "module",
-				message: "请选择模板",
-				choices: [
-					"react",
-					"react-i18n",
-					"react-unocss",
-					"react-i18n-unocss",
-					"vue",
-					"vue-i18n",
-					"vue-unocss",
-					"vue-i18n-unocss"
-				]
-			},
-			{
-				type: "confirm",
-				name: "renameFolder",
-				message: "是否需要为模板重命名？",
-				default: false
-			},
-			{
-				type: "input",
-				name: "folderName",
-				message: "请输入文件夹名字：",
-				when: (answers: { [x: string]: any }) =>
-					answers.renameFolder // 仅当用户选择重命名时显示此问题
-			}
-		];
-		inquirer.prompt(prompts as any[]).then(async answers => {
-			console.log("用户选择:", answers);
-			const spinner = ora();
-			spinner.start("正在匹配...");
+program.command("pull").action(async () => {
+  const prompts = [
+    {
+      type: "list",
+      name: "css",
+      message: "选择哪种CSS预处理器?",
+      choices: ["None", "Sass", "Less"],
+    },
+    {
+      type: "list",
+      name: "framework",
+      message: "选择什么框架?",
+      choices: ["React", "Vue"],
+    },
+    {
+      type: "confirm",
+      name: "i18n",
+      message: "是否使用i18n?",
+    },
+    {
+      type: "confirm",
+      name: "typescript",
+      message: "是否使用typescript?",
+    },
+  ];
+  inquirer.prompt(prompts as any[]).then(async (answers) => {
+    console.log("用户选择:", answers);
+    const spinner = ora();
+    spinner.start("正在匹配...");
 
-			const localPath = path.join(os.tmpdir(), "tarobest-repo"); // 使用临时目录作为本地路径
+    const localPath = path.join(os.tmpdir(), "taro-repo"); // 使用临时目录作为本地路径
 
-			// 确保 localPath 目录存在
-			if (!fs.existsSync(localPath)) fs.mkdirSync(localPath, { recursive: true });
-			
+    // 确保 localPath 目录存在
+    if (!fs.existsSync(localPath)) {
+      fs.mkdirSync(localPath, { recursive: true });
+    }
 
-			// 使用 simpleGit 克隆仓库
-			const git = simpleGit({ baseDir: localPath });
+    // 定义 checkTemplateInfoMatches 函数以比较 package.json 中的 templateInfo
+    const checkTemplateInfoMatches = (
+      packageJsonObj: { [x: string]: any },
+      answers: { [x: string]: any }
+    ) => {
+      const templateInfo = packageJsonObj.templateInfo || {};
+      const lists = ["typescript", "css", "framework", "i18n"];
+      for (const list of lists) {
+        if (templateInfo[list] !== answers[list]) {
+          return false;
+        }
+      }
+      return true;
+    };
 
-			try {
-				// 克隆仓库，但不检出任何分支
-				await git.clone(TEMPLATE_SRC, ".", ["--no-checkout"]);
+    // 使用 simpleGit 克隆仓库
+    const git = simpleGit({ baseDir: localPath });
 
-				// 调用函数并打印结果
-				const branch = answers["module"];
-				await git.checkout(branch);
+    try {
+      // 克隆仓库，但不检出任何分支
+      await git.clone(TEMPLATE_SRC, ".", ["--no-checkout"]);
 
-				const targetFolderName = answers.renameFolder
-					? answers.folderName
-					: branch;
-				const targetPath = path.join(
-					process.cwd(),
-					targetFolderName
-				);
+      const branches = await git.branch(["--all"]);
+      const branchNames = branches.all.map((branch) =>
+        branch.replace(/^remotes\/origin\//, "")
+      );
 
-				spinner.start("正在克隆...");
+      //遍历分支并检查 package.json
+      for (const branch of branchNames) {
+        await git.checkout(branch);
 
-				await git.clone(TEMPLATE_SRC, targetPath, [
-					"--single-branch",
-					"--branch",
-					branch,
-					"--no-tags"
-				]);
-				console.log(`分支 ${branch} 已克隆到 ${targetPath}`);
-				spinner.stop();
-			} catch (error) {
-				console.error("发生错误:", error);
-			} finally {
-				spinner.stop();
-				// 清理临时仓库目录
-				if (fs.existsSync(localPath))
-					await git.clean("f", { cwd: localPath });
+        const packageJsonPath = path.join(localPath, "package.json");
 
-				// 如果不再需要 localPath，可以删除它
-				fs.rmSync(localPath, { recursive: true, force: true });
-			}
-		});
-	});
+        if (fs.existsSync(packageJsonPath)) {
+          const packageJsonContent = fs.readFileSync(packageJsonPath, "utf8");
+          const packageJsonObj = JSON.parse(packageJsonContent);
+
+          if (checkTemplateInfoMatches(packageJsonObj, answers)) {
+            spinner.stop();
+            console.log(chalk.green(`找到匹配的分支: ${branch}`));
+
+            spinner.start("正在克隆...");
+            const targetPath = path.join(process.cwd(), `${branch}`); // 假设你想克隆到当前工作目录
+            await git.clone(TEMPLATE_SRC, targetPath, [
+              "--single-branch",
+              "--branch",
+              branch,
+              "--no-tags",
+            ]);
+            console.log(chalk.green(`分支 ${branch} 已克隆到 ${targetPath}`));
+            spinner.stop();
+            return;
+          }
+        }
+
+        await git.checkout("HEAD");
+        await git.reset(["--hard"]);
+      }
+
+      console.log(chalk.red("没有找到匹配的分支。"));
+    } catch (error) {
+      console.error(chalk.red("发生错误:"), error);
+    } finally {
+      spinner.stop();
+      // 清理临时仓库目录
+      if (fs.existsSync(localPath)) {
+        await git.clean("f", { cwd: localPath });
+        await git.reset(["--hard"]);
+        // 如果不再需要 localPath，可以删除它
+        fs.rmSync(localPath, { recursive: true, force: true });
+      }
+    }
+  });
+});
 
 program.parse();
