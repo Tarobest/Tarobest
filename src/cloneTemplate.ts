af9053758dad6424eb6e5ac421458f16a5f06398
xiyueyezibile <2859439384@qq.com>
2024-08-28 16:16:41 +0800

    feat: 优化克隆过程

--- src/cloneTemplate.ts
+++ src/cloneTemplate.ts
@@ -1,76 +1,61 @@
-import chalk from "chalk";
 import path from "path";
-import simpleGit from "simple-git";
 import os from "os";
-import fs from "fs";
+import fs from "fs-extra";
 import { Answers } from "./types/cli";
 import ora from "ora";
 import { Git } from "./model/git";
 import { ROOT_DIR } from "./constants";
+import { print } from "./model/print";
+import { Genarate } from "./model/genarate";
 
-const TEMPLATE_SRC = "git@github.com:jia8708/Tarobest.git";
+const TEMPLATE_SRC = "git@github.com:Tarobest/Tarobest.git";
 
 export const cloneTemplate = async (answers: Answers) => {
 	console.log("用户选择:", answers);
 	const spinner = ora();
-	spinner.start("正在匹配...");
-	const localPath = path.join(os.tmpdir(), "taro-repo"); // 使用临时目录作为本地路径
-
-	// 确保 localPath 目录存在
-	if (!fs.existsSync(localPath)) {
-		fs.mkdirSync(localPath, { recursive: true });
+	const temporarilyDir = path.join(ROOT_DIR, answers.name);
+	if (!fs.existsSync(temporarilyDir)) {
+		// 创建临时目录
+		await fs.ensureDir(temporarilyDir);
+	} else {
+		print.red.error("项目已存在");
+		process.exit(1);
 	}
+	spinner.start("正在克隆...");
 
-	const git = new Git(TEMPLATE_SRC, localPath, answers);
+	const git = new Git(TEMPLATE_SRC, temporarilyDir, answers);
+	const genarate = new Genarate(answers);
 
+	await cloneBranch(git, spinner, answers, async () => {
+		await genarate.genaratePkg();
+	});
+};
+// 克隆分支
+async function cloneBranch(
+	git: Git,
+	spinner: ora.Ora,
+	answers: Answers,
+	callback: any = () => {}
+) {
 	try {
-		// 克隆仓库，但不检出任何分支
-		await git.clone(".", ["--no-checkout"]);
+		const targetPath = path.join(ROOT_DIR, `${answers.name}`); // 假设你想克隆到当前工作目录
 
+		await git.clone(targetPath, ["--no-checkout"]);
 		const branchNames = await git.getBranches();
+		const branch = branchNames.filter(b => b === answers.template)[0];
 
-		//遍历分支并检查 package.json
-		for (const branch of branchNames) {
-			if (branch === answers.template) {
-				await git.checkout(branch);
+		spinner.stop();
 
-				spinner.stop();
-				console.log(chalk.green(`找到匹配的分支: ${branch}`));
+		await git.ensureBranch(branch);
+		await callback();
+		spinner.stop();
 
-				spinner.start("正在克隆...");
-				const targetPath = path.join(ROOT_DIR, `${answers.name}`); // 假设你想克隆到当前工作目录
-				await git.clone(targetPath, [
-					"--single-branch",
-					"--branch",
-					branch,
-					"--no-tags"
-				]);
-				console.log(
-					chalk.green(`分支 ${branch} 已克隆到 ${targetPath}`)
-				);
-				spinner.stop();
-				return;
-			}
-			// }
+		print.green.log(`分支 ${branch} 已克隆到 ${targetPath}`);
 
-			await git.checkout("HEAD");
-			await git.reset();
-		}
-
-		console.log(chalk.red("没有找到匹配的分支。"));
+		await git.reset();
 	} catch (error) {
-		console.error(chalk.red("发生错误:"), error);
+		print.red.error(`克隆失败: ${error}`);
 	} finally {
 		spinner.stop();
-		// 清理临时仓库目录
-		if (fs.existsSync(localPath)) {
-			await git.clean();
-			await git.reset(["--hard"]);
-			// 如果不再需要 localPath，可以删除它
-			fs.rmSync(localPath, {
-				recursive: true,
-				force: true
-			});
-		}
 	}
-};
+}
--- src/model/git.ts
+++ src/model/git.ts
@@ -1,28 +1,36 @@
-import simpleGit, { SimpleGit } from "simple-git";
+import simpleGit, { SimpleGit, TaskOptions } from "simple-git";
 import { Answers } from "../types/cli";
+import fs from "fs-extra";
+import path from "path";
 
 export class Git {
 	private _url: string; // 仓库地址
 	private _temporarilyDir: string; // 临时目录
 	private _git: SimpleGit;
-	private _answer: Answers;
+	// private _answer: Answers;
 	constructor(url: string, dir: string, answer: Answers) {
 		this._url = url;
 		this._temporarilyDir = dir;
 		this._git = simpleGit({
 			baseDir: dir
 		});
-		this._answer = answer;
+		// this._answer = answer;
 	}
-	async clone(path: string, options: string[] = []) {
+	async clone(path: string, options: TaskOptions = []) {
 		await this._git.clone(this._url, path, options);
 	}
 	async getBranches() {
 		const branches = await this._git.branch(["--all"]);
-		const branchNames = branches.all.map(branch =>
-			branch.replace(/^remotes\/origin\//, "")
-		);
+
+		const branchNames = branches.all
+			.filter(branch => branch.startsWith("remotes/origin/"))
+			.map(branch => branch.replace(/^remotes\/origin\//, ""));
 		return branchNames;
+	}
+	// 确保分支存在
+	async ensureBranch(branch: string) {
+		await this._git.checkout(branch);
+		await fs.remove(path.join(this._temporarilyDir, ".git"));
 	}
 	async checkout(branch: string) {
 		await this._git.checkout(branch);
