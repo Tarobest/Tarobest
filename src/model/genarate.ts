8e2ae8af0e8c4cb04bc109947a937c0f04d598ed
xiyueyezibile <2859439384@qq.com>
2024-08-28 16:39:38 +0800

    feat: 修复代码bug，实现genarate api

--- src/model/genarate.ts
+++ src/model/genarate.ts
@@ -1,22 +1,43 @@
 import fs from "fs-extra";
 import { Answers } from "../types/cli";
 import { ROOT_DIR } from "../constants";
-import {print} from "./print";
+import { print } from "./print";
 import path from "path";
 
-export class Genarate {
+export abstract class Genarate {
 	private _answer: Answers;
 	private _root: string;
-	constructor(answer: Answers, root: string = ROOT_DIR) {
+	constructor(
+		answer: Answers,
+		root: string = path.join(ROOT_DIR, answer.name)
+	) {
 		this._answer = answer as Answers;
 		this._root = root;
 	}
-	public async genaratePkg() {
-		const { name, description, author } = this._answer;
-		if (fs.existsSync(path.join(this._root, name, "package.json"))) {
+	getAnswer() {
+		return this._answer;
+	}
+	getRoot() {
+		return this._root;
+	}
+	abstract genaratePkg(): Promise<any>;
+	abstract genaratePages(): Promise<any>;
+}
+
+export class GenarateReact extends Genarate {
+	constructor(
+		answer: Answers,
+		root: string = path.join(ROOT_DIR, answer.name)
+	) {
+		super(answer, root);
+	}
+	async genaratePkg() {
+		const { name, description, author } = super.getAnswer();
+		if (fs.existsSync(path.join(super.getRoot(), "package.json"))) {
 			print.green.log(
-				`找到package.json ${path.join(this._root, name, "package.json")}`
+				`找到package.json ${path.join(super.getRoot(), "package.json")}`
 			);
 		}
 	}
+	async genaratePages() {}
 }
